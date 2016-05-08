8025ad884438a12b497600c39b9a2920e8e74c9c
Maurice-Marie <mauricestromer@googlemail.com>
2016-05-08 22:10:06 +0200

    Vertically scrolling background
    Inifinite vertically scrolling background. Based on externally set
    viewport

--- core/src/com/peppercarrot/runninggame/entities/Background.java
+++ core/src/com/peppercarrot/runninggame/entities/Background.java
@@ -3,100 +3,81 @@ package com.peppercarrot.runninggame.entities;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.peppercarrot.runninggame.PaCGame;
 import com.peppercarrot.runninggame.utils.Constants;
 
 /**
- * An infinitely scrolling and repeating background.
- * Contains two rectangles which are connected horizontally seemless.
- * When the second rectangle is about to hit left bound of the screen,
- * the first rectangle will be applied right after the second, so by
- * further scrolling the image is displayed continuously.
+ * An infinitely scrolling and repeating background. The viewport has to be set
+ * from a higher level, so that this entity only renders the resulting tiles.
+ * 
+ * TODO: If the stage consist of a single camera which is used to define the
+ * background position, this could be placed as an actor within that stage.
+ * 
  * @author WinterLicht
+ * @author momsen
  *
  */
-public class Background extends Actor{
+public class Background {
 
-	private int width; /** Width of the image in pixel. */
-	private int height; /** Height of the image in pixel. */
-	private final TextureRegion textureRegion; /** Repeating image. */
-	private Rectangle textureRegionBounds1; /** Controlling rectangle in dimensions of the image. */
-	private Rectangle textureRegionBounds2; /** Controlling rectangle in dimensions of the image. */
-	private int speed = -350; /** Scroll speed in x-direction in pixel. */
+	private final int textureWidth;
 
-	public Background(int speed) {
-		//Scroll speed of background is lower than this of level
-		this.speed =(int) (-speed*0.4);
-		//TODO: name of the texture image as a parameter?
-		Texture tex = new Texture(Gdx.files.internal("testbg.png"), true);
-		textureRegion = new TextureRegion(tex);
-		width = tex.getWidth();
-		height = tex.getHeight();
-		textureRegionBounds1 = new Rectangle(- width / 2, 0, height, height);
-		textureRegionBounds2 = new Rectangle(width / 2, 0, width, height);
+	private final int textureHeight;
+
+	private final Texture texture;
+
+	private final int columnsIfCentered;
+
+	private final int rowsIfCentered;
+
+	private final float centerOffsetY;
+
+	public Background(String backgroundPicture) {
+		texture = new Texture(Gdx.files.internal(backgroundPicture), true);
+		texture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+		textureWidth = texture.getWidth();
+		textureHeight = texture.getHeight();
+
+		centerOffsetY = -Constants.VIRTUAL_HEIGHT / 2.0f;
+
+		columnsIfCentered = Constants.VIRTUAL_WIDTH / textureWidth
+				+ (Constants.VIRTUAL_WIDTH % textureWidth > 0 ? 1 : 0);
+
+		rowsIfCentered = Constants.VIRTUAL_HEIGHT / textureHeight
+				+ (Constants.VIRTUAL_HEIGHT % textureHeight > 0 ? 1 : 0);
 	}
 
-		@Override
-		public void act(float delta) {
-			if (leftBoundsReached(delta)) {
-				resetBounds();
-			} else {
-				updateXBounds(delta);
-				updateYBounds();
+	/**
+	 * Renders a viewport clip of the background starting at a specific
+	 * position.
+	 * 
+	 * @param batch
+	 *            Sprite batch to render. Begin and End have to be called
+	 *            elsewhere.
+	 * @param x
+	 *            position of the viewport clip to render
+	 * @param y
+	 *            position of the viewport clip to render
+	 */
+	public void draw(Batch batch, float x, float y) {
+		float clipX = -(x % textureWidth);
+		float clipY = (y % textureHeight);
+
+		int columns = columnsIfCentered + (clipX != 0 ? 1 : 0);
+
+		int rowOffset = (int) (y / textureHeight);
+		int rows = rowsIfCentered + (clipY != 0 ? 1 : 0);
+
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < columns; col++) {
+				// Base camera does not alter the x position, the clip will be
+				// scrolled
+				float tileX = clipX + col * textureWidth;
+
+				// Base camera does alter the y position, the clip will not be
+				// scrolled but framed
+				float tileY = (row + rowOffset) * textureHeight + centerOffsetY;
+
+				batch.draw(texture, tileX, tileY, textureWidth, textureHeight);
 			}
 		}
-
-		@Override
-		public void draw(Batch batch, float parentAlpha) {
-			super.draw(batch, parentAlpha);
-			batch.draw(textureRegion, textureRegionBounds1.x, textureRegionBounds1.y, width,
-					height);
-			batch.draw(textureRegion, textureRegionBounds2.x, textureRegionBounds2.y, width,
-					height);
-			/*
-			batch.draw(textureRegion, textureRegionBounds1.x, textureRegionBounds1.y, Constants.VIRTUAL_WIDTH,
-					Constants.VIRTUAL_HEIGHT);
-			batch.draw(textureRegion, textureRegionBounds2.x, textureRegionBounds2.y, Constants.VIRTUAL_WIDTH,
-					Constants.VIRTUAL_HEIGHT);
-			*/
-		}
-
-		/**
-		 * Is the second rectangle about to hit left bound of the screen.
-		 * @param delta timedelta
-		 * @return
-		 */
-		private boolean leftBoundsReached(float delta) {
-			return (textureRegionBounds2.x <= 0);
-		}
-
-		/**
-		 * Scroll horizontally.
-		 * @param delta
-		 */
-		private void updateXBounds(float delta) {
-			textureRegionBounds1.x +=  speed;
-			textureRegionBounds2.x +=  speed;
-		}
-		
-		/**
-		 * Update vertically.
-		 */
-		private void updateYBounds(){
-			textureRegionBounds1.y = PaCGame.getInstance().camera.position.y - Constants.VIRTUAL_HEIGHT / 2;
-			textureRegionBounds2.y = textureRegionBounds1.y; 
-		}
-
-		/**
-		 * Attach rectangles to each other horizontally.
-		 */
-		private void resetBounds() {
-			textureRegionBounds1 = textureRegionBounds2;
-			textureRegionBounds2 = new Rectangle(width, 0, width, height);
-			//textureRegionBounds2 = new Rectangle(Constants.VIRTUAL_WIDTH, 0, Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT);
-		}
-
+	}
 }
--- core/src/com/peppercarrot/runninggame/screens/WorldScreen.java
+++ core/src/com/peppercarrot/runninggame/screens/WorldScreen.java
@@ -1,8 +1,8 @@
 package com.peppercarrot.runninggame.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.peppercarrot.runninggame.PaCGame;
@@ -11,22 +11,30 @@ import com.peppercarrot.runninggame.stages.WorldStage;
 import com.peppercarrot.runninggame.utils.Constants;
 
 /**
- * Screen for actual game.
- * In render() method are keyboard inputs handled. Handles also the
- * main game camera.
- * TODO: Main camera is alligned according the player position, also
- * other camera should consider the offset.
- * TODO: background in world stage as attribute?
+ * Screen for actual game. In render() method are keyboard inputs handled.
+ * Handles also the main game camera. TODO: Main camera is alligned according
+ * the player position, also other camera should consider the offset. TODO:
+ * background in world stage as attribute?
+ * 
  * @author WinterLicht
  *
  */
-public class WorldScreen implements Screen {
+public class WorldScreen extends ScreenAdapter {
 	WorldStage stage;
-	Background background; /** Infinitely scrolling background. */
+
+	/**
+	 * TODO: Should be calculated within the background, but since there are no
+	 * seemingless scrolling cameras at the moment, this is just incremented by
+	 * scroll-speed.
+	 */
+	private float backgroundScrollX = 0.0f;
+
+	/** Infinitely scrolling background. */
+	private final Background background;
 
 	public WorldScreen(Viewport viewport) {
 		stage = new WorldStage(viewport);
-		background = new Background((int) stage.level.scrollSpeed);
+		background = new Background("testbg.png");
 	}
 
 	@Override
@@ -37,39 +45,34 @@ public class WorldScreen implements Screen {
 	@Override
 	public void render(float delta) {
 		PaCGame game = PaCGame.getInstance();
-		//Update main game camera.
-		//Main camera is placed in the middle of the game screen,
-		//but moves vertically when player jumps.
-		//TODO: Let the main camera unchanged and add instead
-		//a special camera to render player???
+		// Update main game camera.
+		// Main camera is placed in the middle of the game screen,
+		// but moves vertically when player jumps.
+		// TODO: Let the main camera unchanged and add instead
+		// a special camera to render player???
 		game.camera.position.set(Constants.VIRTUAL_WIDTH / 2,
-				stage.runner.getY()+Constants.VIRTUAL_HEIGHT / 2 - Constants.OFFSET_TO_GROUND, 0);
+				stage.runner.getY() + Constants.VIRTUAL_HEIGHT / 2 - Constants.OFFSET_TO_GROUND, 0);
 		game.camera.update();
 		game.batch.setProjectionMatrix(game.camera.combined);
 
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		game.batch.begin();
-		game.batch.setColor(1, 1, 1, 1);
-		background.act(delta);
-		background.draw(game.batch, 1f);
-		game.batch.end();
-
+		renderBackground(game);
 		this.stage.render(delta);
 
-		//TODO: Current keyboard controls are as follow:
-		if(Gdx.input.isKeyJustPressed(Keys.BACK)|| Gdx.input.isKeyPressed(Keys.ESCAPE)){
+		// TODO: Current keyboard controls are as follow:
+		if (Gdx.input.isKeyJustPressed(Keys.BACK) || Gdx.input.isKeyPressed(Keys.ESCAPE)) {
 			Gdx.app.exit();
 		}
-		if(Gdx.input.isKeyJustPressed(Keys.C)){
+		if (Gdx.input.isKeyJustPressed(Keys.C)) {
 			stage.runner.activateAbility(1);
 		}
-		if(Gdx.input.isKeyJustPressed(Keys.X)){
+		if (Gdx.input.isKeyJustPressed(Keys.X)) {
 			stage.runner.activateAbility(2);
 		}
-		if(Gdx.input.isKeyJustPressed(Keys.Y)){
+		if (Gdx.input.isKeyJustPressed(Keys.Y)) {
 			stage.runner.activateAbility(3);
 		}
-		if(Gdx.input.isKeyJustPressed(Keys.SPACE)){
+		if (Gdx.input.isKeyJustPressed(Keys.SPACE)) {
 			if (!stage.playerReady) {
 				stage.level.beginLevel = true;
 			}
@@ -77,34 +80,17 @@ public class WorldScreen implements Screen {
 		}
 	}
 
+	private void renderBackground(PaCGame game) {
+		game.batch.begin();
+		game.batch.setColor(1, 1, 1, 1);
+		background.draw(game.batch, backgroundScrollX, stage.runner.getY());
+		backgroundScrollX += stage.level.scrollSpeed;
+		game.batch.end();
+	}
+
 	@Override
 	public void resize(int width, int height) {
 		PaCGame.getInstance().viewport.update(width, height);
 		stage.getViewport().update(width, height, true);
 	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
